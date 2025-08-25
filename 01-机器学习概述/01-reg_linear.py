f4ea0626f25276382ad975d05cb45557cd82916d
archiecheng <archiechengice@outlook.com>
2025-08-25 09:58:37 -0700

    线性回归模型预测学生成绩 - 模型导入和导出使用

--- 01-机器学习概述/01-reg_linear.py
+++ 01-机器学习概述/01-reg_linear.py
@@ -12,7 +12,7 @@ y = [84.2, 80.6, 80.1, 90, 83.2, 87.6, 79.4, 93.4]
 """
 # 1. 导入依赖包
 from sklearn.linear_model import LinearRegression
-
+import joblib
 
 def dm01_regres():
     # 2. 准备数据
@@ -26,6 +26,13 @@ def dm01_regres():
     print(f'regression.intercept_ --> {regression.intercept_}')
     # 5. 模型预测
     print(regression.predict([[80, 86]]))
+    # 6. 模型保存
+    joblib.dump(regression, '../model/mymodel2.bin')
+    # 7. 模型加载
+    estimator1 = joblib.load('../model/mymodel2.bin')
+    # 8. 模型再预测
+    print(estimator1.predict([[90, 86]]))
+
 
 if __name__ == '__main__':
     dm01_regres()
